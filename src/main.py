d3dab3f3ccd02b063fe70c056b8ae50ad49459b1
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-03-04 21:57:35 +0900

    feat: todo List

--- src/main.py
+++ src/main.py
@@ -2,6 +2,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def health_check_handler():
     return {"status": "ok"}
+
+
+todo_data = {
+    1: {"id": 1, "contents": "buy milk", "is_done": True},
+    2: {"id": 2, "contents": "buy bread", "is_done": False},
+    3: {"id": 3, "contents": "buy egg", "is_done": False},
+}
+
+
+@app.get("/todos")
+def get_todos_handler(order: str | None = None):
+    ret = list(todo_data.values())
+
+    if order == "desc":
+        return ret[::-1]
+    else:
+        return ret
