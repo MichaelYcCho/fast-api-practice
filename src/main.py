2887c2454b700763df624235db62dd6c7c8ecab5
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-03-14 23:23:23 +0900

    feat: env config

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+import os
 from fastapi import Body, FastAPI, HTTPException
 from pydantic import BaseModel
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 app = FastAPI()
 
