278c574661baded6a833d22eb2fcd1b1a921451e
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-03-27 11:21:13 +0900

    test: Get Todos

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -13,3 +13,25 @@ def test_health_check():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
+
+def test_get_todos():
+    response = client.get("/todos")
+    assert response.status_code == 200
+    assert response.json() == {
+        "todos": [
+            {"id": 1, "contents": "buy milk", "is_done": True},
+            {"id": 2, "contents": "buy bread", "is_done": True},
+            {"id": 3, "contents": "buy egg", "is_done": True},
+        ]
+    }
+
+    response = client.get("/todos?order=DESC")
+    assert response.status_code == 200
+    assert response.json() == {
+        "todos": [
+            {"id": 3, "contents": "buy egg", "is_done": True},
+            {"id": 2, "contents": "buy bread", "is_done": True},
+            {"id": 1, "contents": "buy milk", "is_done": True},
+        ]
+    }
