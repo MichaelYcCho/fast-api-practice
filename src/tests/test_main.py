290226357dd41fe6170fafc1cd2f345ce8a8548d
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-03-28 12:18:52 +0900

    test: Mocking

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -15,7 +15,17 @@ def test_health_check():
     assert response.json() == {"status": "ok"}
 
 
-def test_get_todos():
+def test_get_todos(mocker):
+
+    mocker.patch(
+        "main.get_todos",
+        return_value=[
+            {"id": 1, "contents": "buy milk", "is_done": True},
+            {"id": 2, "contents": "buy bread", "is_done": True},
+            {"id": 3, "contents": "buy egg", "is_done": True},
+        ],
+    )
+
     response = client.get("/todos")
     assert response.status_code == 200
     assert response.json() == {
