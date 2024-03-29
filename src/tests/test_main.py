0ed6d1d308be8ea4459d74de7e55509073c7d733
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2024-03-29 11:21:13 +0900

    test: Test Get Todo

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -34,3 +34,30 @@ def test_get_todos(client, mocker):
             {"id": 1, "contents": "buy milk", "is_done": True},
         ]
     }
+
+
+def test_get_todo(client, mocker):
+    """
+    # 200 test
+    개별 테스트 : pytest src/tests/test_main.py::test_get_todo
+    """
+
+    mocker.patch(
+        "main.get_todo_by_todo_id",
+        return_value={"id": 1, "contents": "buy milk", "is_done": True},
+    )
+    response = client.get("/todos/1")
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "contents": "buy milk", "is_done": True}
+
+
+def test_get_todo_not_found(client, mocker):
+    """
+    # 404 test
+    개별 테스트 : pytest src/tests/test_main.py::test_get_todo_not_found
+    """
+
+    mocker.patch("main.get_todo_by_todo_id", return_value=None)
+    response = client.get("/todos/1")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Todo not found"}
